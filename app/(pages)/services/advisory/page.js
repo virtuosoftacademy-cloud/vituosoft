e798922e5d41bb69d1c6d95c473b94da80207fdd
Virtuosoft <virtuosoftacademy@gmail.com>
2026-02-06 11:37:02 +0500

    chgs

--- app/(pages)/services/advisory/page.js
+++ app/(pages)/services/advisory/page.js
@@ -6,6 +6,7 @@ import ValueSection from './_components/ValueSection'
 import Success from './_components/Success'
 import Empower from './_components/Empower'
 import Engage from './_components/Engage'
+import TimeLine from './_components/TimeLine'
 
 function Advisory() {
     return (
@@ -16,6 +17,7 @@ function Advisory() {
                     <ValueSection />
                     <div className='px-16 md:px-26'>
                         <Success />
+                        {/* <TimeLine/> */}
                         <Empower />
                     </div>
                 </div>
