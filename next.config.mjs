df581c03b6cacb2b8b8ed920c88cae3b5b1a47c6
Virtuosoft <virtuosoftacademy@gmail.com>
2026-02-16 11:09:02 +0500

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath:"https://virtuosoft.pk/virtuosoft",
-  assetPrefix:"https://virtuosoft.pk/virtuosoft/",
   output:"standalone",
   images: {
     unoptimized: true
